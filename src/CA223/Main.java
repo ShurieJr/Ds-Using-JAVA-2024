6712eb7de2458956797f1a97395153b0be7d74e8
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2024-09-16 16:17:05 +0300

    Ds using java

--- src/CA223/Main.java
+++ src/CA223/Main.java
@@ -2,6 +2,7 @@ package CA223;
 
 public class Main {
     public static void main(String[] args) {
+
         //create object from Circle class
         Circle myCircle = new Circle();
         //myCircle.setRadius(10);
@@ -9,6 +10,7 @@ public class Main {
         System.out.println("Radius: " + myCircle.radius);
         System.out.println("Area: " + myCircle.getArea());
         System.out.println("---------------------------");
+
         //create object2 from Circle class
         Circle yourCircle = new Circle(4);
         System.out.println("Radius: " + yourCircle.radius);
